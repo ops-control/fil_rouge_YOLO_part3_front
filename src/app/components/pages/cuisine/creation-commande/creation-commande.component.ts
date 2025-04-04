966da2cb71e649d4d35746b727311e7fe3b3f967
red one <153833587+ops-control@users.noreply.github.com>
2025-04-04 16:32:33 +0200

    adding the cuisine folder

---
+++ src/app/components/pages/cuisine/creation-commande/creation-commande.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-commande',
+  imports: [],
+  templateUrl: './creation-commande.component.html',
+  styleUrl: './creation-commande.component.css'
+})
+export class CreationCommandeComponent {
+
+}
