c49c9122dd3054a75276d986227968e868acc5da
regisaberbache <regis.aberbache@gmail.com>
2025-04-04 15:50:44 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'fil_rouge_YOLO_part3_front';
+}
