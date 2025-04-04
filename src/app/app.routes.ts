1f6bf85fc54a2c06a204063e2901a479ec210945
regisaberbache <regis.aberbache@gmail.com>
2025-04-04 16:37:47 +0200

    initial route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './components/pages/accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: AccueilComponent},
+];
