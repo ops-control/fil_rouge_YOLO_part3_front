611e2112f4869f3cd18e96a30deffc3a88a0081a
Maud Gauthier <maud.gauthier@gmail.com>
2025-04-07 15:50:38 +0200

    item-reservation.component & listReservations.component done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { AccueilComponent } from './components/pages/accueil/accueil.component';
+import { ReservationComponent } from './components/pages/reservation/liste-reservations/listeReservations.component';
+import { CreerReservationComponent } from './components/pages/reservation/creer-reservation/creer-reservation.component';
 
 export const routes: Routes = [
     { path: "", component: AccueilComponent},
+    { path: "reservations", component: ReservationComponent},
+    { path: "reservations/creer-resa", component: CreerReservationComponent},
 ];
--- src/app/components/pages/reservation/creer-reservation/creer-reservation.component.ts
+++ src/app/components/pages/reservation/creer-reservation/creer-reservation.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReservationService } from '../../../../services/reservation.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creer-reservation',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './creer-reservation.component.html',
   styleUrl: './creer-reservation.component.css'
 })
 export class CreerReservationComponent {
+  formNewReservation: FormGroup
+
+  constructor(
+    private reservationService: ReservationService,
+    private router: Router
+  ) {
+    this.formNewReservation = new FormGroup({
+      nbPersonne: new FormControl("", Validators.required),
+      horaireReservation: new FormControl("", Validators.required)
+      
+    });
+  }
+
+  saveReservation() {
+    if (this.formNewReservation.valid) {
+      this.reservationService.addReservation(this.formNewReservation.value).subscribe(response => {
+        this.router.navigate(["/reservations"]);
+      });
+    }
+  }
+
+  display_error(field : string, error : string) {
+    return this.formNewReservation?.get(field)?.dirty
+        && this.formNewReservation?.get(field)?.errors?.[error];
+  }
 
 }
