46f77a23c0078000b1ddefdfc2d7675cec8948d2
regisaberbache <regis.aberbache@gmail.com>
2025-04-08 16:16:47 +0200

    add observer interface and allow list-tables-occupee component to refresh when table servie

--- src/app/components/pages/serviceEnSalle/modal-modification-statut-commande-en-servie/modal-modification-statut-commande-en-servie.component.ts
+++ src/app/components/pages/serviceEnSalle/modal-modification-statut-commande-en-servie/modal-modification-statut-commande-en-servie.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TableOccupee } from '../../../../interfaces/table-occupee';
+import { CommandeCreationService } from '../../../../services/commande-creation.service';
 
 @Component({
   selector: 'app-modal-modification-statut-commande-en-servie',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './modal-modification-statut-commande-en-servie.component.html',
   styleUrl: './modal-modification-statut-commande-en-servie.component.css'
 })
 export class ModalModificationStatutCommandeEnServieComponent {
+  commandeCreationService: CommandeCreationService;
+
+  @Input()
+  table?: TableOccupee
+
+  @Output()
+  closeModal: EventEmitter<TableOccupee> = new EventEmitter<TableOccupee>();
+
+  constructor(commandeCreationService : CommandeCreationService){
+    this.commandeCreationService = commandeCreationService;
+  }
+
+  confirmer() {
+    console.log(this.table?.numeroTable)
+    // ici je dois appeler Commandes service qui doit passer la commande en servie
+    this.commandeCreationService.updateCommandeToServie(this.table?.idCommande).subscribe(() =>{
+      this.commandeCreationService.notify();
+    })
+    this.closeModal.emit(this.table);
+  }
+
+  fermerModal() {
+    this.closeModal.emit();
+  }
 
 }
--- src/app/components/pages/serviceEnSalle/table-occupee-item/table-occupee-item.component.ts
+++ src/app/components/pages/serviceEnSalle/table-occupee-item/table-occupee-item.component.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TableOccupee } from '../../../../interfaces/table-occupee';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table-occupee-item',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './table-occupee-item.component.html',
   styleUrl: './table-occupee-item.component.css'
 })
 export class TableOccupeeItemComponent {
   @Input()
   tableOccupee?: TableOccupee;
+  @Output()
+  emitTable : EventEmitter<TableOccupee> = new EventEmitter<TableOccupee>();
+
+  selectionnerTable(tableOccupee : TableOccupee) {
+    this.emitTable.emit(tableOccupee);
+  }
 
 }
