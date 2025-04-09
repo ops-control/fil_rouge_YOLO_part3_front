12b5255ec023680cc4b200cd9b7ec1cf4b3b1b58
CharrVal <charriervalentin2@gmail.com>
2025-04-09 14:40:15 +0200

    header update

--- src/app/components/global/header/header.component.ts
+++ src/app/components/global/header/header.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  
+  constructor(private router : Router) { }
+
+  estConnecte(): boolean {
+    return sessionStorage.getItem('utilisateur') != null;
+  }
+
+  get_prenom() {
+    return sessionStorage.getItem("utilisateur");
+  }
+
+  // deconnecter(): void {
+  //   sessionStorage.removeItem('utilisateur');
+  //   this.router.navigate(['/accueil']);
+  // }
 
 }
