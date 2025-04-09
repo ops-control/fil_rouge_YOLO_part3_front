66d069448b81b2b680c77d0c9f518ab51790ade6
CharrVal <charriervalentin2@gmail.com>
2025-04-09 15:21:26 +0200

    header modification for connexion user

--- src/app/components/pages/connexion/connexion.component.ts
+++ src/app/components/pages/connexion/connexion.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { Utilisateur } from '../../../interfaces/utilisateur';
 
 @Component({
   selector: 'app-connexion',
@@ -13,6 +14,7 @@ import { AuthService } from '../../../services/auth.service';
 export class ConnexionComponent {
   form : FormGroup;
   errorMessage: string | null = null;
+  utilisateur : Utilisateur = {'prenom': "valentin", 'nom' : "Charrier" };
 
   constructor(private fb : FormBuilder,
               private router : Router,
@@ -31,6 +33,7 @@ export class ConnexionComponent {
       .subscribe({
         next: response => {
           localStorage.setItem('Jwt', response.token);
+          sessionStorage.setItem("utilisateur", this.utilisateur.prenom)
           this.router.navigate(['/accueil']);
         },
         error: () => {
